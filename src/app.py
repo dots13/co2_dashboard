4ecfcb6bbc9d11490984ce6e1256bcb811209850
dots_13 <stasya.dots@gmail.com>
2023-08-09 04:17:17 +0200

    DashTools Automatic Push - 08/09/2023 @ 04:17:17

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import numpy as np
 from dash import Dash, dcc, html, Input, Output
 from dash import jupyter_dash
 import os
+import pickle
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 
@@ -56,7 +57,7 @@ rel_path_model = "models/model_co2.pkl"  # the target file
 rel_to_cwd_path_model = os.path.join(script_dir, rel_path_model)  # the cwd-relative path of the target file
 
 # Load model
-loaded = ARIMAResults.load(rel_to_cwd_path_model)
+loaded = pickle.load(open(rel_to_cwd_path_model, 'rb'))
 
 colors = {'background': '#111111',
           'text': '#7FDBFF'}
